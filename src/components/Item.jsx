65e8c21c87d9c6e4a678543acef8991bb149b64c
Cristian Dalmaso <c.dalmaso3@gmail.com>
2022-03-23 18:04:58 -0300

    Modifica estilos para 1ra entrega

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -10,16 +10,13 @@ const Item = ({producto}) => {
   return (
     <Link to={`detalle/${producto.id}`} >
     <div className="item-individual">
-        {<Card style={{ width: "auto"}}>
-        <Card.Img variant="top" src={producto.imagen}/>
+        {<Card>
+        <Card.Img variant="top" src={producto.imagen} className="imagenProducto"/>
         <Card.Body>
           {/* <Card.Title id>Id producto: {producto.id}</Card.Title> */}
-          <Card.Title nombre> {producto.nombre} </Card.Title>
+          <Card.Title nombre>{producto.nombre} </Card.Title>
           <Card.Text precio>${producto.precio}</Card.Text>
-          <Button variant="primary">Añadir al carrito</Button>
-          <br></br>
-          <br></br>
-          <Button variant="primary">Eliminar del carrito</Button>
+          <Button variant="success">Añadir al carrito</Button>
         </Card.Body>
       </Card>}
     </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,26 +1,34 @@
 import React from 'react'
-import logo from '../images/logo.png'
+import logo from '../images/logo-v2.png'
 import navBar from '../css/navBar.css'
 import CartWidget from './CartWidget.jsx';
 import Navbar  from "react-bootstrap/Navbar";
 import Nav  from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 function NavBar() {
   return (
     
-    <Navbar bg="dark" variant="dark">
-    <Container>
-    <Navbar.Brand href="#home"><img src={logo} className="logo-imagen"/><br></br>Hood FSH</Navbar.Brand>
-
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Container>
+  <Navbar.Brand href="#home"><img src={logo} className="logo-imagen"></img></Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="me-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <Nav.Link href="#hoodies">Hoodies</Nav.Link>
+      <Nav.Link href="#jackets">Jackets</Nav.Link>
+      <Nav.Link href="#sneakers">Sneakers</Nav.Link>
+      <Nav.Link href="#sneakers">Remeras</Nav.Link>
     </Nav>
-    </Container>
-    <CartWidget/> 
-  </Navbar> 
+    <Nav>
+      <Nav.Link eventKey={2} href="#cart">
+      <CartWidget/>
+      </Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+  </Container>
+</Navbar>
   )
 }
 export default NavBar;
