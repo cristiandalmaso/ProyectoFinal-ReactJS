d831679e89e32a9dc778fee58cca58d7bc7c5f84
Cristian Dalmaso <c.dalmaso3@gmail.com>
2022-04-22 21:25:13 -0300

    Updates

---
+++ precache-manifest.88a9c16a3ba8c36b867eb014871a9248.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d68a6181a6f745cea66a",
+    "url": "/ProyectoFinal-ReactJS/static/css/main.bc290b0a.chunk.css"
+  },
+  {
+    "revision": "d68a6181a6f745cea66a",
+    "url": "/ProyectoFinal-ReactJS/static/js/main.d68a6181.chunk.js"
+  },
+  {
+    "revision": "897aa20ba8584a8a9d67",
+    "url": "/ProyectoFinal-ReactJS/static/js/1.897aa20b.chunk.js"
+  },
+  {
+    "revision": "d74500c972a51ca60b02",
+    "url": "/ProyectoFinal-ReactJS/static/css/2.213b3ccd.chunk.css"
+  },
+  {
+    "revision": "d74500c972a51ca60b02",
+    "url": "/ProyectoFinal-ReactJS/static/js/2.d74500c9.chunk.js"
+  },
+  {
+    "revision": "72393f768f0cef1b97d3",
+    "url": "/ProyectoFinal-ReactJS/static/js/runtime~main.72393f76.js"
+  },
+  {
+    "revision": "89f979dbcb5c1a6d74ad60a12d185f31",
+    "url": "/ProyectoFinal-ReactJS/static/media/logo-v2.89f979db.png"
+  },
+  {
+    "revision": "9b0a1f78dda59056d587eef17802c9dd",
+    "url": "/ProyectoFinal-ReactJS/index.html"
+  }
+];
